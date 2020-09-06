81b6aff63bdfe066fc6b4b0b8b64d93d09ee6680
lionel <nkeoualionel@gmail.com>
2020-09-06 17:37:48 +0200

    create submit activity

---
+++ app/src/main/java/com/lionelnkeoua/pluralsight1/view/activity/SubmitActivity.java
@@ -0,0 +1,16 @@
+package com.lionelnkeoua.pluralsight1.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lionelnkeoua.pluralsight1.R;
+
+public class SubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+}
