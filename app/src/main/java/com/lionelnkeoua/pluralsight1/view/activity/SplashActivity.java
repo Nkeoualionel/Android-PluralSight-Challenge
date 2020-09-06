f69368c8e03a0915abb3a13fc50b5a25ad0822e3
lionel <nkeoualionel@gmail.com>
2020-09-06 13:25:41 +0200

    added handler to splash

--- app/src/main/java/com/lionelnkeoua/pluralsight1/view/activity/SplashActivity.java
+++ app/src/main/java/com/lionelnkeoua/pluralsight1/view/activity/SplashActivity.java
@@ -1,21 +1,44 @@
 package com.lionelnkeoua.pluralsight1.view.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.lionelnkeoua.pluralsight1.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    @BindView(R.id.text)
-    TextView textView;
+    @BindView(R.id.mImg)
+    ImageView mImageView;
+
+    private static int time = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, time);
     }
+
 }
