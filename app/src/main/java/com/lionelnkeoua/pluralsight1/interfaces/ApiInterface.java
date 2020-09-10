baf3d750424dd1ab423390674b5e112cffeacc5f
lionel <nkeoualionel@gmail.com>
2020-09-11 00:21:23 +0200

    added alertDialogue

--- app/src/main/java/com/lionelnkeoua/pluralsight1/interfaces/ApiInterface.java
+++ app/src/main/java/com/lionelnkeoua/pluralsight1/interfaces/ApiInterface.java
@@ -21,7 +21,7 @@ public interface ApiInterface {
     Call<List<Skill>> getSkills();
 
     @FormUrlEncoded
-    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formRespons")
     Call<ResponseBody>  insertData( @Field("entry.1877115667")String firstname,
                                     @Field("entry.2006916086")String lastname,
                                     @Field("entry.1824927963")String email,
--- app/src/main/java/com/lionelnkeoua/pluralsight1/view/activity/SubmitActivity.java
+++ app/src/main/java/com/lionelnkeoua/pluralsight1/view/activity/SubmitActivity.java
@@ -9,10 +9,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.lionelnkeoua.pluralsight1.R;
 import com.lionelnkeoua.pluralsight1.api.ApiForm;
 import com.lionelnkeoua.pluralsight1.interfaces.ApiInterface;
@@ -31,11 +36,15 @@ public class SubmitActivity extends AppCompatActivity {
     @BindView(R.id.mGithubProjetLink)
     EditText mGithubProjetLink;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
-
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Project Submit");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         ButterKnife.bind(this);
     }
 
@@ -70,18 +79,22 @@ public class SubmitActivity extends AppCompatActivity {
 
                     switch (response.code()){
                         case 200:
-                            Toast.makeText(SubmitActivity.this, "AJOUT EFFECTUEE", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SubmitActivity.this, "SUCCES ADD", Toast.LENGTH_LONG).show();
+                            alerteDialogue(R.drawable.succes);
                             break;
                         case 404:
                             Toast.makeText(SubmitActivity.this, "NOT FUND", Toast.LENGTH_LONG).show();
+                            alerteDialogue(R.drawable.faild);
                             break;
 
                         case 500:
-                            Toast.makeText(SubmitActivity.this, "ERREUR SERVEUR", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SubmitActivity.this, "ERROR SERVER", Toast.LENGTH_LONG).show();
+                            alerteDialogue(R.drawable.faild);
                             break;
 
                         default:
-                            Toast.makeText(SubmitActivity.this, "ECHEC AJOUT", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SubmitActivity.this, "FAILD ADD", Toast.LENGTH_LONG).show();
+                            alerteDialogue(R.drawable.faild);
                             break;
                     }
             }
@@ -91,5 +104,20 @@ public class SubmitActivity extends AppCompatActivity {
                 Toast.makeText(SubmitActivity.this, "ERREUR INTERNET", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+
+    public void alerteDialogue(int image){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SubmitActivity.this);
+        final View dialogView = LayoutInflater.from(SubmitActivity.this).inflate(R.layout.server_response, null);
+        dialogBuilder.setView(dialogView);
+
+        final ImageView img =  dialogView.findViewById(R.id.mImgResponseServer);
+        Glide.with(SubmitActivity.this)
+                .load(image)
+                .into(img);
+
+        AlertDialog b = dialogBuilder.create();
+        b.show();
     }
 }
